de191f4d3918f3d88512423ebec299a774c966fd
Vaclav Volhejn <vaclav@kyutai.org>
2025-09-29 10:49:49 +0200

    Make wikibooks dataset bigger, .gitignore it

--- scripts/process_wikibooks.py
+++ scripts/process_wikibooks.py
@@ -6,8 +6,8 @@ from pathlib import Path
 import regex
 import tqdm
 
-TARGET_TEXT_LENGTH = 3000
-N_TEXTS = 16
+TARGET_TEXT_LENGTH = 7500
+N_TEXTS = 32
 
 REPO_ROOT = Path(__file__).parents[1]
 
@@ -58,7 +58,7 @@ def get_filtered_texts(dataset_path: Path):
 
             newline_chars = text.count("\n") + text.count("\r")
             percent_newlines = (newline_chars / len(text) * 100) if len(text) > 0 else 0
-            special_chars = len(regex.findall(r"[#\$<>@/\-%\+]", text))
+            special_chars = len(regex.findall(r"[#\$<>@/\-%\+\*\(\)]", text))
             percent_special = (special_chars / len(text) * 100) if len(text) > 0 else 0
 
             if percent_special + percent_newlines > 1:
@@ -78,14 +78,26 @@ def get_filtered_texts(dataset_path: Path):
     return texts_filtered
 
 
-def main():
-    dataset = []
+def text_to_turns(text: str) -> list[str]:
+    # Note this discards the last bit if it doesn't end with punctuation
+    turns = [
+        # Regex explanation: anything (non-greedy),
+        # then '.', '!', or '?',
+        # followed by a space or newline so that e.g. "1.23" doesn't split the sentence
+        p.strip()
+        for p in regex.findall(r"(.*?[\.\!\?](?:\s|\n))", text)
+        if p.strip()
+    ]
+    return turns
 
-    for language in ["en", "fr"]:
+
+def main():
+    for language in ["fr", "en"]:
         filtered_texts = get_filtered_texts(DATASET_PATHS[language])
         filtered_texts = filtered_texts[:N_TEXTS]
         speakers = SPEAKERS[language]
 
+        dataset = []
         for (i_speaker, (speaker, gender)), (i_text, text) in itertools.product(
             enumerate(speakers), enumerate(filtered_texts)
         ):
@@ -94,8 +106,7 @@ def main():
                     "id": f"{language}_speaker_{i_speaker}_text_{i_text}",
                     "speaker_audios": [speaker],
                     "tags": ["wikibooks", f"gender={gender}", "monologue"],
-                    # Having everything in one turn is fine for DSM
-                    "turns": [text],
+                    "turns": text_to_turns(text),
                     "language": language,
                 }
             )
@@ -103,6 +114,9 @@ def main():
         with (REPO_ROOT / "datasets" / f"wikibooks_{language}.jsonl").open("w") as f:
             for item in dataset:
                 f.write(json.dumps(item, ensure_ascii=False) + "\n")
+            print(
+                f"Wrote {len(dataset)} samples to {REPO_ROOT / 'datasets' / f'wikibooks_{language}.jsonl'}"
+            )
 
 
 if __name__ == "__main__":
